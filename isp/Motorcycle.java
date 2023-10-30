f31c99a33cae02c457ea56ea3a0284c364f45553
DESKTOP-58C7LJE\User <meizia.raihan@gmail.com>
2023-10-30 15:06:43 +0700

    commit isp

---
+++ isp/Motorcycle.java
@@ -0,0 +1,16 @@
+public class Motorcycle implements VehicleInterface{
+	@Override
+	public void drive() {
+	
+	}
+	
+    @Override
+	public void stop() {
+		
+	}
+	
+	@Override
+	public void refuel() {
+		
+	}
+}
